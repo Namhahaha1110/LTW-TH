40dee369ec778ff38fb0cbb9f8d0861a25651131
Namhahaha1110 <hnam.11102005@gmail.com>
2026-01-23 10:53:01 +0700

    Buoi 02 - EF Core, Repository, SeedData

--- SportsSln/SportsStore/Program.cs
+++ SportsSln/SportsStore/Program.cs
@@ -1,11 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
+
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<StoreDbContext>(opts =>
+{
+    opts.UseSqlServer(builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
+});
 
 var app = builder.Build();
 
 app.UseStaticFiles();
-
 app.MapDefaultControllerRoute();
+
+SeedData.EnsurePopulated(app);
 
 app.Run();
