7a6903ec1d08d5c3bf2573e3b904be6043feb677
Namhahaha1110 <23dh114467@st.huflit.edu.vn>
2026-01-30 10:12:37 +0700

    Buoi 3 - Phan trang

--- SportsSln/SportsStore/Models/SeedData.cs
+++ SportsSln/SportsStore/Models/SeedData.cs
@@ -17,7 +17,7 @@ namespace SportsStore.Models
             if (!context.Products.Any())
             {
                 context.Products.AddRange(
-                    new Product { Name = "Kayak", Description = "A boat for one person", Category = "Watersports", Price = 275 },
+                    new Product { Name = "Kayak", Description = "A boat for one person", Category = "Watersports", Price = 500000000 },
                     new Product { Name = "Lifejacket", Description = "Protective and fashionable", Category = "Watersports", Price = 48.95m },
                     new Product { Name = "Soccer Ball", Description = "FIFA-approved size and weight", Category = "Soccer", Price = 19.50m },
                     new Product { Name = "Corner Flags", Description = "Give your playing field a professional touch", Category = "Soccer", Price = 34.95m },
